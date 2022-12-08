cbb38b7d68fcec7fa8ad3a57c91c4510a2326409
Hannan <hannansultan@outlook.com>
2022-12-08 23:06:19 +0500

    Initialization check

--- teams-sdk/src/App.tsx
+++ teams-sdk/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { app } from "@microsoft/teams-js";
 
 function App() {
+  app
+    .initialize()
+    .then(() => {
+      console.log("Initialized Teams SDK");
+    })
+    .catch((error) => console.log("error initializing Teams SDK", error));
+  if (app.isInitialized()) {
+    console.log("Teams SDK initializing success");
+  }
   return (
     <div className="App">
       <header className="App-header">
